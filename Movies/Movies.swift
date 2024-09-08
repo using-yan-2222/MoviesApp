6b7d73e5c7b90cb458e700546ce77ba8a41938aa
using-yan-2222 <dfsaboce@gmail.com>
2024-09-08 02:37:23 -0400

    add the nextMovie method in the movies class

--- Movies/Movies.swift
+++ Movies/Movies.swift
@@ -10,5 +10,16 @@ import Foundation
 class Movies{
     private var index = 0;
     let movie_dic : [String: String] = ["Back to the Future" : "Michael J. Fox", "Interstellar" : "Matthew McConaughey", "The Matrix" : "Keanu Reeves", "Twelve Monkeys" : "Bruce Willis", "District 9" : "Sharlto Copley", "Inception": "Leonardo DiCaprio", "The Butterfly Effect":"Ashton Kutcher", "Ghost in the Shell":"Atsuko Tanaka"]
+    private var movieTitles: [String]
+    
+    init(){
+        self.movieTitles = Array(movie_dic.keys)
+    }
+    
+    func nextMovie() -> String? {
+        let movie = movieTitles[index]
+        index = (index + 1) % movieTitles.count
+        return movie
+    }
     
 }
