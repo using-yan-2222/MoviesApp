f83f760bcf51a32c61badd839c99a99108e92440
using-yan-2222 <dfsaboce@gmail.com>
2024-09-08 13:01:05 -0400

    Finish the content views and rewrite the movies class for clearer code

--- Movies/ContentView.swift
+++ Movies/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    private var title = "Movie"
+    private var title = "Sci-Fi Movie"
+    @State private var movieTitle: String = "Interstellar"
+    var movies = Movies()
+  
     var body: some View {
         VStack {
             Text(title)
+                .font(.largeTitle)
+                .foregroundColor(.green)
+            Spacer()
+            Text(movieTitle)
+                .font(.title)
+                .padding()
+            Spacer()
+            
+            Button("Next"){
+                movieTitle = movies.nextMovie()
+            }
+            .font(.title)
         }
         .padding()
+            
+        
     }
+    
 
     
 }
--- Movies/Movies.swift
+++ Movies/Movies.swift
@@ -8,25 +8,33 @@
 import Foundation
 
 class Movies{
-    private var index = 0;
-    let movie_dic : [String: String] = ["Michael J. Fox": "Back to the Future",
-                                        "Matthew McConaughey": "Interstellar",
-                                        "Keanu Reeves": "The Matrix",
-                                        "Bruce Willis": "Twelve Monkeys",
-                                        "Sharlto Copley": "District 9",
-                                        "Leonardo DiCaprio": "Inception",
-                                        "Ashton Kutcher": "The Butterfly Effect",
-                                        "Atsuko Tanaka": "Ghost in the Shell"]
-    private var movieTitles: [String]
+        
+    private var movies : [String: String] = [
+        "Michael J. Fox": "Back to the Future",
+        "Matthew McConaughey": "Interstellar",
+        "Keanu Reeves": "The Matrix",
+        "Bruce Willis": "Twelve Monkeys",
+        "Sharlto Copley": "District 9",
+        "Leonardo DiCaprio": "Inception",
+        "Ashton Kutcher": "The Butterfly Effect",
+        "Atsuko Tanaka": "Ghost in the Shell"
+    ]
+    private var index: Int = 0;
+  
+   
     
-    init(){
-        self.movieTitles = Array(movie_dic.values)
+    private var movieTitles: [String] {
+            return Array(movies.values)
     }
     
-    func nextMovie() -> String? {
+    
+    func nextMovie() -> String {
+        
         let movie = movieTitles[index]
         index = (index + 1) % movieTitles.count
         return movie
     }
-    
+    func currentMovie() -> String{
+        return movieTitles[index];
+    }
 }
