9d86919a6c5f350eb278d7489d1044e868d80be3
using-yan-2222 <dfsaboce@gmail.com>
2024-09-08 02:19:34 -0400

    add movie_dic

--- Movies/Movies.swift
+++ Movies/Movies.swift
@@ -2,7 +2,13 @@
 //  Movies.swift
 //  Movies
 //
-//  Created by Albert Zou on 9/8/24.
+//  Created by Yan Zou on 9/8/24.
 //
 
 import Foundation
+
+class Movies{
+    private var index = 0;
+    let movie_dic : [String: String] = ["Back to the Future" : "Michael J. Fox", "Interstellar" : "Matthew McConaughey", "The Matrix" : "Keanu Reeves", "Twelve Monkeys" : "Bruce Willis", "District 9" : "Sharlto Copley", "Inception": "Leonardo DiCaprio", "The Butterfly Effect":"Ashton Kutcher"]
+    
+}
